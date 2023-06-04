bee3ecf7192d53d506694eb4607f68a24bdcff61
Mark <edwardsdeveloping.now@gmail.com>
2023-06-04 16:41:20 -0500

    simplify structure

--- db/models.py
+++ db/models.py
@@ -14,6 +14,15 @@ class Project(Base):
         orm_mode = True
 
 
+class Document(Base):
+    __tablename__ = 'documents'
+    id = Column(Integer, primary_key=True)
+    project_id = Column(String(30))
+    image_path = Column(String(50))
+    summary = Column(String(1200))
+    tags = Column(ARRAY(String))
 
+    class Config:
+        orm_mode = True
 
 Base.metadata.create_all(engine)
